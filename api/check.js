07cbc3efbb6afb6ec1a16f69b88c1feb08d07df1
lucafiumara74-pixel <lucafiumara74@gmail.com>
2026-05-04 15:19:36 +0200

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,1 +1,17 @@
+const https = require('https');
 
+module.exports = function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  var domain = req.query.domain || '';
+
+  https.get('https://rdap.org/domain/' + domain, function(r) {
+    var data = '';
+    r.on('data', function(c) { data += c; });
+    r.on('end', function() {
+      if (r.statusCode === 404) return res.json({ available: true });
+      return res.json({ available: false });
+    });
+  }).on('error', function(e) {
+    return res.json({ available: null, error: e.message });
+  });
+};
